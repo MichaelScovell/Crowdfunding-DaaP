4f245b922fbff38c6515937bc7f31283b9840f58
MichaelScovell <michael.scovell@cevo.com.au>
2023-02-02 17:01:09 +1100

    Updated config and thirdweb scripts

--- crowdfund-contracts/hardhat.config.js
+++ crowdfund-contracts/hardhat.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
+    defaultNetwork: 'goreli',
+    networks: {
+      hardhat: {},
+      goerli: {
+        url: process.env.GOERLI_RPC_URL,
+        accounts: [process.env.MW_PRIVATE_KEY]
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
