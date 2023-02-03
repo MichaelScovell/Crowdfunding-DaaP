01fe0f2fe1f773d48d69fb53a71bfd4fb6c99865
MichaelScovell <michael.scovell@cevo.com.au>
2023-02-03 22:39:43 +1100

    updated index.html and started web3 logic wrapping in frontend

---
+++ client/src/context/index.js
@@ -0,0 +1,7 @@
+// Index Js page for Smart Contract Interactions (Web 3 Logic)
+
+// Defining lib imports
+import React, {useContext, createContext} from 'react';
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
+import { ethers } from 'ethers';
+
