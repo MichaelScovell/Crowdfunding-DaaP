cf5719eaea8d6d294ac6cc4915885322b47a4efe
MichaelScovell <michael.scovell@cevo.com.au>
2023-02-03 14:54:34 +1100

    Integrated Tailwind CSS and started defining routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 // Defining our imports
+
+// Packages and libraries
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
+// File imports
 import App from "./App";
+import "./index.css";
 
 // Creating a root
 const root = ReactDOM.createRoot(document.getElementById("root"));
